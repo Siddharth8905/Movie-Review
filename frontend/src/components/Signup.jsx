dbb17b451d4cd9da1204f85f2123c114e39431f3
Siddharth8905 <siddsrini2@gmail.com>
2025-09-23 10:17:11 +0530

    upadated signup

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { useState } from 'react'
+import {Link,useNavigate} from 'react-router-dom'
+import axios from 'axios'
+
 
 export default function Signup() {
+
+    const [name, setname] = useState("")
+    const [email, setemail] = useState("")
+    const [password, setpassword] = useState("")
+    const navigate=useNavigate()
+
+    const savedata=async(e)=>{
+        try {
+            const data={name,email,paasword}
+            const s=await axios.post("",data)
+            const r=s.save()
+            console.log(r.data);
+            navigate("/login")
+            
+        } catch (error) {
+            console.log({ msg: error.message });
+        }
+
+    }
   return (
     <div>
         <form>
             <h1>SignUp</h1>
-            <input type='text' placeholder='Name'/><br/>
-            <input type='email' placeholder='Email'/><br/>
-            <input type='password' placeholder='Password'/><br/>
-            <button >Signup</button>
+            <input type='text' placeholder='Name'required onChange={(e)=>(setname(e.target.value))}/><br/>
+            <input type='email' placeholder='Email' required onChange={(e)=>(setemail(e.target.value))}/><br/>
+            <input type='password' placeholder='Password'required onChange={(e)=>(setpassword(e.target.value))} /><br/>
+            <button  onClick={savedata}>Signup</button>
             <p> Already a User ? <Link to="/login">Login</Link></p>
         </form>
     </div>
